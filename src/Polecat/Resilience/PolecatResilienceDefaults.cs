d64b794749602fafd6bcab3e75f87af017d8fd75
Dmytro Pryvedeniuk <dmytro.pryvedeniuk@gmail.com>
2026-04-16 00:21:17 +0300

    Fix remaining warnings, remove redundant comment

--- src/Polecat/Resilience/PolecatResilienceDefaults.cs
+++ src/Polecat/Resilience/PolecatResilienceDefaults.cs
@@ -2,9 +2,6 @@ using Polly;
 
 namespace Polecat.Resilience;
 
-/// <summary>
-///     Default Polly resilience pipeline transient error handling.
-/// </summary>
 internal static class PolecatResilienceDefaults
 {
     public static ResiliencePipelineBuilder AddPolecatDefaults(this ResiliencePipelineBuilder builder)
